92f462e6f3168f7cbb8d6b95418151ded674db02
Mohammed Jubel <jubelm@hotmail.co.uk>
2019-03-08 12:48:37 +0000

    updated Navbar added further links

--- ncknews-fe/src/components/Navbar.jsx
+++ ncknews-fe/src/components/Navbar.jsx
@@ -6,29 +6,29 @@ function Navbar({ topics }) {
   return (
     <nav className="navbar">
       <ul>
-        <Link onClick={() => window.location.reload()} to="/">
+        <Link onClick={() => window.location.reload()} to="/articles">
           <li>
             <b>NC NEWS</b>
           </li>
         </Link>
+
         <li>
           PICK TOPIC
           <ul className="dropDown">
             {topics.map(topic => {
               return (
-                <Link
-                  onClick={() => window.location.reload()}
-                  to={`/topics/${topic.slug}`}
-                  key={topic.slug}
-                >
+                <Link to={`/topics/${topic.slug}`} key={topic.slug}>
                   <li>{topic.slug}</li>
                 </Link>
               );
             })}
           </ul>
         </li>
-        <Link onClick={() => window.location.reload()} to="/articles/post">
-          <li className="post">POST ARTICLE</li>
+        <Link onClick={() => window.location.reload()} to="/article/post">
+          <li>NEW POST</li>
+        </Link>
+        <Link onClick={() => window.location.reload()} to="/">
+          <li className="post">LOGIN</li>
         </Link>
       </ul>
     </nav>
