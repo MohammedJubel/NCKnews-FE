f095f8bb29a4d78a4ad79119c5a7c4af62380bb9
MohammedJubel <jubelm@hotmail.co.uk>
2019-04-10 13:29:37 +0100

    Made Changes to Api request layout/ tidying

--- ncknews-fe/src/api.js
+++ ncknews-fe/src/api.js
@@ -1,76 +1,87 @@
 import axios from "axios";
 
-const BASE_URL = "https://nc-knews-mohammedjubel.herokuapp.com/api";
+const request = axios.create({
+  baseURL: "https://nc-knews-mohammedjubel.herokuapp.com/api"
+});
 
 export const getArticles = async sort_by => {
-  const { data } = await axios.get(
-    `${BASE_URL}/articles?limit=100000&sort_by=${sort_by}`
-  );
-  return data;
-};
-
-export const getArticlesByTopic = async topic => {
-  const { data } = await axios.get(
-    `${BASE_URL}/articles?topic=${topic}&limit=100`
-  );
-  return data;
+  const { data } = await request.get("/articles", {
+    params: {
+      sort_by: [sort_by],
+      limit: 10,
+      page: 1
+    }
+  });
+  return data.articles;
 };
 
 export const getArticleById = async article_id => {
-  const { data } = await axios.get(`${BASE_URL}/articles/${article_id}`);
+  const { data } = await request.get(`/articles/${article_id}`);
   return data.article;
 };
 
-export const getTopics = async () => {
-  return await axios.get(`${BASE_URL}/topics`).then(topics => {
-    return topics.data.topics;
+export const getArticlesByTopic = async topic => {
+  const { data } = await request.get(`/articles?topic=${topic}`, {
+    params: {
+      limit: 100
+    }
   });
+  return data.articles;
 };
 
-export const getComments = async article_id => {
-  const { data } = await axios.get(
-    `${BASE_URL}/articles/${article_id}/comments`
-  );
+export const getTopics = async () => {
+  const { data } = await request.get("/topics");
+  return data.topics;
+};
+
+export const postTopic = async topic => {
+  const { data } = await request.post("/topics", topic);
   return data;
 };
 
+export const getComments = async article_id => {
+  const { data } = await request.get(`/articles/${article_id}/comments`);
+  return data.comments;
+};
+
 export const postNewComment = async (article_id, commentToAdd) => {
-  console.log(article_id, commentToAdd, "<---This");
-  const { data } = await axios.post(
-    `${BASE_URL}/articles/${article_id}/comments`,
+  const { data } = await request.post(
+    `/articles/${article_id}/comments`,
     commentToAdd
   );
   return data.comment;
 };
 
-export const postArticle = newArticle => {
-  console.log(newArticle, "api new Article");
-  return axios
-    .post(`${BASE_URL}/articles`, newArticle)
-    .then(({ data }) => data.article);
+export const postArticle = async newArticle => {
+  const { data } = await request.post(`/articles`, newArticle);
+  return data.article;
 };
 
 export const getSingleUser = async username => {
-  const { data } = await axios.get(`${BASE_URL}/users/${username}`);
+  const { data } = await request.get(`/users/${username}`);
   return data.user;
 };
 
-export const deleteCommentById = comment_id => {
-  return axios.delete(`${BASE_URL}/comments/${comment_id}`);
+export const getAllUsers = async () => {
+  const { data } = await request.get("/users");
+  return data.users;
+};
+
+export const deleteCommentById = async comment_id => {
+  const { data } = await request.delete(`/comments/${comment_id}`);
+  return data;
 };
 
 export const deleteArticleById = async article_id => {
-  console.log(BASE_URL);
-  const res = await axios.delete(`${BASE_URL}/articles/${article_id}`);
-  console.log(res);
+  const { data } = await request.delete(`/articles/${article_id}`);
+  return data;
 };
 
 export const voteOnItem = async (comment_id, article_id, voteChange) => {
-  console.log(comment_id, "comment_id");
   const URL = comment_id
-    ? `${BASE_URL}/comments/${comment_id}`
-    : `${BASE_URL}/articles/${article_id}`;
-  const { data } = await axios.patch(URL, {
+    ? `/comments/${comment_id}`
+    : `/articles/${article_id}`;
+  const { data } = await request.patch(URL, {
     inc_votes: voteChange
   });
   return data.article;
