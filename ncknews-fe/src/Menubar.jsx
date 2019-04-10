6b053674d1ebd696e7ffffa2eb5b9833467f59be
MohammedJubel <jubelm@hotmail.co.uk>
2019-04-10 13:29:15 +0100

    Created Menubar Component

---
+++ ncknews-fe/src/Menubar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Nav, NavItem, NavLink } from "reactstrap";
+
+export default class Menubar extends React.Component {
+  render() {
+    return (
+      <div>
+        <Nav>
+          <NavLink href="#">Link</NavLink> <NavLink href="#">Link</NavLink>{" "}
+          <NavLink href="#">Another Link</NavLink>{" "}
+          <NavLink disabled href="#">
+            Disabled Link
+          </NavLink>
+        </Nav>
+      </div>
+    );
+  }
+}
